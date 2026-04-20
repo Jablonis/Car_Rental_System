163e1e0949e85e0589270952ac5b10a44b2e8a7d
Patrik <patrik@MacBook-Pro-uzivatela-Patrik.local>
2026-04-20 20:30:20 +0200

    Priprava na Railway deploy

--- src/data/db.ts
+++ src/data/db.ts
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "autosalon_db",
+  host: process.env.MYSQLHOST || process.env.DB_HOST || "127.0.0.1",
+  port: Number(process.env.MYSQLPORT || process.env.DB_PORT || 3306),
+  user: process.env.MYSQLUSER || process.env.DB_USER || "root",
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || "",
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME || "autosalon_db",
 });
 
 export default pool;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import dotenv from "dotenv";
 import publicRoutes from "./routes/public.route.js";
 import pool from "./data/db.js";
 import session from "express-session";
@@ -7,7 +8,7 @@ import authRoutes from "./routes/auth.route.js";
 import { checkAuthStatus } from "./middleware/check-auth.middleware.js";
 import adminRoutes from "./routes/admin.route.js";
 import carRoutes from "./routes/car.route.js";
-import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
@@ -15,23 +16,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// views
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "src", "views"));
 
-// static assets
 app.use(
   "/assets",
   express.static(path.join(process.cwd(), "src", "views", "public", "assets")),
 );
+
 app.use(
-session({
-  secret: process.env.SESSION_SECRET || "fallback-secret",
-  resave: false,
-  saveUninitialized: false,
-}),
+  session({
+    secret: process.env.SESSION_SECRET || "fallback-secret",
+    resave: false,
+    saveUninitialized: false,
+  }),
 );
-// routes
+
 app.use(checkAuthStatus);
 
 app.use("/", publicRoutes);
@@ -46,8 +46,8 @@ async function startServer() {
     const [rows] = await pool.query("SELECT 1");
     console.log("DB connected:", rows);
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
     console.error("DB connection failed:", error);
